adb1a496c0ada0d2edc190457354079ace81987c
Saarika <55930906+saarikabhasi@users.noreply.github.com>
2021-08-27 14:25:43 -0400

    Create  Find N Unique Integers Sum up to Zero.py

---
+++ leetcode/easy/Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,44 @@
+"""
+
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+Example 2:
+
+Input: n = 3
+Output: [-1,0,1]
+Example 3:
+
+Input: n = 1
+Output: [0]
+ 
+
+Constraints:
+
+1 <= n <= 1000
+
+"""
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        if n == 1:
+            return [0]
+        else:
+            r = []
+            if n %2 !=0:
+                r.append(0)
+            
+            i = 1
+            while len(r)<n:
+                r.append(0-i)
+                r.append(i)
+                i+=1
+            return r
+                    
+                
